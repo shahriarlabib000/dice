20c0d64faf8c2a8b25680e93a3c9779405d8b263
shahriarlabib000 <shahriarlabib000@gmail.com>
2024-08-09 17:28:48 +0600

    more

--- main.gd
+++ main.gd
@@ -16,14 +16,22 @@ func _physics_process(_delta):
 		$dice.apply_torque($dice.basis.x * randi_range(-500,500))
 		$dice.apply_torque($dice.basis.y * randi_range(-600,600))
 		$dice.apply_torque($dice.basis.z * randi_range(-460,460))
+	if Input.is_action_just_pressed("gone") and $img.visible==false:
+		$img.show()
+		$boom.play()
+		$dice.process_mode=Node.PROCESS_MODE_DISABLED
+		$dice.position= Vector3(0,5,-3)
+		$dice.process_mode=Node.PROCESS_MODE_INHERIT
+		$dice.apply_torque($dice.basis.x * 5)
+		await get_tree().create_timer(.1).timeout
+		$img.hide()
+		
 	for Ray:RayCast3D in $dice/rays.get_children():
 		if Ray.is_colliding():
 			if Ray.get_collider().is_in_group("floor"):
 				$number.text= Ray.name
 
 
-func _on_gone_pressed():
-	$dice.process_mode=Node.PROCESS_MODE_DISABLED
-	$dice.position= Vector3(0,5,-3)
-	$dice.process_mode=Node.PROCESS_MODE_INHERIT
-	$dice.apply_torque($dice.basis.x * 5)
+func _on_area_3d_body_entered(body):
+	if body.is_in_group("wall"):
+		$dice/AudioStreamPlayer3D.play()
